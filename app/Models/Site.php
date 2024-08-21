d707f2600c6ef8eef09820d892bf23d7d9ab6db3
dhananjaydevmonkey <dhananjaya.k@devmonkey.in>
2024-08-21 14:41:01 +0530

    Login check

--- app/Models/Site.php
+++ app/Models/Site.php
@@ -60,8 +60,8 @@ class Site extends Model
         return DB::select("SELECT COUNT(*) cnt FROM enduser WHERE email='$data[emailAddress]';");
     }
 
-    public function getUserAuthenticate($data){
-        return DB::select("SELECT CONCAT(first_name,' ',last_name) user_name,id FROM enduser WHERE email='$data[email]' AND pword='$data[pword]';");
+    public function getUserLoginCheck($data){
+        return DB::select("SELECT CONCAT(first_name,' ',last_name) user_name,id FROM enduser WHERE email='$data[email]' AND pword='$data[password]';");
     }
 
     public function saveAppointments($data){
